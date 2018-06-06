1b408bdbcd313413536f06997b477809382c3341
Zura Guerra <guerrazuramx@gmail.com>
2018-06-05 19:29:14 -0500

    Tune(?) Ocarina

--- src/components/audio/Instrument.js
+++ src/components/audio/Instrument.js
@@ -5,11 +5,11 @@ export default class Instrument extends Component {
   render() {
     return (
       <div>
-        <Note instrument={this.props.name} note="D" dataKey="119" />
-        <Note instrument={this.props.name} note="F" dataKey="97" />
-        <Note instrument={this.props.name} note="A" dataKey="115" />
-        <Note instrument={this.props.name} note="B" dataKey="100" />
-        <Note instrument={this.props.name} note="D2" dataKey="103" />
+        <Note instrument={this.props.name} note="D2" dataKey="119" />
+        <Note instrument={this.props.name} note="B" dataKey="97" />
+        <Note instrument={this.props.name} note="D" dataKey="115" />
+        <Note instrument={this.props.name} note="A" dataKey="100" />
+        <Note instrument={this.props.name} note="F" dataKey="103" />
       </div>
     )
   }
